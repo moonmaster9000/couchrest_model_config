abf2a96276899f5b4c00492f7f704f33cc32ac23
Matt Parker <moonmaster9000@gmail.com>
2011-05-02 10:57:53 -0400

    beefing up the reset.feature test coverage

--- features/reset.feature
+++ features/reset.feature
@@ -1,6 +1,6 @@
 Feature: Resetting the configuration
   
   Scenario: Resetting configuration
-    Given I have configured the databases
+    Given I have configured CouchRest::Model via CouchRest::Model::Config
     When I call the reset method
-    Then the couchrest_model database configuration should be reset to defaults
+    Then CouchRest::Model::Config should be reset to defaults 
--- features/step_definitions/reset_steps.rb
+++ features/step_definitions/reset_steps.rb
@@ -1,19 +1,35 @@
-Given /^I have configured the databases$/ do
+Given /^I have configured CouchRest::Model via CouchRest::Model::Config$/ do
   class Book < CouchRest::Model::Base; end
-  CouchRest::Model::Config.Book.all.should be_nil
   CouchRest::Model::Config.edit do
     model Book do
-      all "book_db" 
+      default "book_db" 
+    end
+
+    environment do
+      "haha"
+    end
+
+    server do
+      default "http://poo.com"
     end
   end
-  CouchRest::Model::Config.Book.all.class.should == CouchRest::Database
-  CouchRest::Model::Config.Book.all.name.should == "book_db"
+  CouchRest::Model::Config.Book.default.class.should == CouchRest::Database
+  CouchRest::Model::Config.Book.default.name.should == "book_db"
+  CouchRest::Model::Config.server.default.uri.should == "http://poo.com"
+  CouchRest::Model::Config.environment.should == "haha"
 end
 
 When /^I call the reset method$/ do
   CouchRest::Model::Config.reset
 end
 
-Then /^the couchrest_model database configuration should be reset to defaults$/ do
+Then /^CouchRest::Model::Config should be reset to defaults$/ do
+  unless defined? Rails
+    Rails = double "Rails"
+  end
+  Rails.stub(:env).and_return "matt"
   CouchRest::Model::Config.Book.all.should be_nil
+  CouchRest::Model::Config.Book.default.should be_nil
+  CouchRest::Model::Config.server.default.uri.should == "http://127.0.0.1:5984"
+  CouchRest::Model::Config.environment.should == "matt"
 end
