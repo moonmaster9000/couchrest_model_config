e832f5d0c9459e6e16f66856743db0e17b32f839
Matt Parker <moonmaster9000@gmail.com>
2011-05-12 17:23:04 -0400

    v0.1.3
      -- simpler method for fixing rails reloading problem now in a railtie.

--- lib/couchrest_model_config.rb
+++ lib/couchrest_model_config.rb
@@ -3,3 +3,4 @@ require 'couchrest_model_config/config'
 require 'couchrest_model_config/model'
 require 'couchrest_model_config/server'
 require 'couchrest_model_config/couchrest_model_base.rb'
+require 'couchrest_model_config/railtie' if defined?(Rails)
---
+++ lib/couchrest_model_config/railtie.rb
@@ -0,0 +1,31 @@
+module CouchRest
+  module Model
+    module Config
+      class Railtie < ::Rails::Railtie
+        config.to_prepare do
+          module ::CouchRest
+            module Model
+              class Base
+                class_eval <<-EOS, __FILE__, __LINE__ + 1
+                  def self.database
+                    CouchRest::Model::Config.for(self).current_database
+                  end
+                  
+                  def database
+                    CouchRest::Model::Config.for(self.class).current_database
+                  end
+                EOS
+                
+                class << self
+                  def use_database(*args)
+                    raise "We're sorry, but the `use_database` method is not supported with couchrest_model_config."
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
