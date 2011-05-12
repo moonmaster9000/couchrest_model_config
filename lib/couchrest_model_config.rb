c6a8a8edc9979faa63eca04ef8dd8c24eae15c68
Matt Parker <moonmaster9000@gmail.com>
2011-05-12 14:39:13 -0400

    only create inheritable attributes if they don't already exist

--- lib/couchrest_model_config.rb
+++ lib/couchrest_model_config.rb
@@ -1,4 +1,5 @@
 require 'couchrest_model'
+require 'couchrest_model_config/couchrest_model_inheritable_attributes'
 require 'couchrest_model_config/config'
 require 'couchrest_model_config/model'
 require 'couchrest_model_config/server'
